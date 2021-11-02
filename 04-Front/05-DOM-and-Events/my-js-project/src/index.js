80482f8d568fc7535af296e7bdfe32b803d2db27
juliette guerbette <turtleju@gmail.com>
2021-11-02 10:51:14 +0100

    DOM and Events Lecture

---
+++ 04-Front/05-DOM-and-Events/my-js-project/src/index.js
@@ -0,0 +1,19 @@
+const winners = document.querySelectorAll("#fifa-wins li");
+const list = document.querySelector('#fifa-wins');
+
+setTimeout(() => {
+  list.insertAdjacentHTML('beforeend', '<li>France (2 wins)</li>');
+}, 2000);
+
+const home = document.getElementById("home");
+
+console.log(home.innerText);
+console.log(home.innerHTML);
+console.log(home.attributes.href.value);
+
+document.querySelectorAll("img").forEach((img) => {
+  img.addEventListener("click", (event) => {
+    debugger
+    event.currentTarget.classList.toggle("img-circle");
+  });
+});
